3b3d2da8a95a6d3e6196eb139ac6b9f9195c40e3
iamparasghimire <iamparasghimire@gmail.com>
2023-08-04 15:57:11 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react"
+import Header from "./components/header"
+import Meme from "./components/meme"
+
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Project</h4>
+        </header>
+    )
+}
